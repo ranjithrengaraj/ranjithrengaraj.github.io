94f51259c19b1185987239f0db7d2694e92f121b
ranjithrengaraj <52474397+ranjithrengaraj@users.noreply.github.com>
2019-11-10 23:07:59 -0800

    Add files via upload

---
+++ static/test.js
@@ -0,0 +1,54 @@
+
+
+function checkSlience() {
+  console.log("-checkSlience----");
+  //Microphone demo
+ // var getUserMedia = require('getusermedia'); 
+  //var attachmediastream = require('attachmediastream');
+  //var notification = document.querySelector('#userSpeaking');
+ //var log = bows('Microphone Demo');
+
+  const constraints = window.constraints = {
+    audio: true,
+    video: false
+  };
+  
+
+
+navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+ // getUserMedia(function(err, stream) {
+   // if (err) throw err
+
+    //attachmediastream(stream, document.querySelector('#mic video'));
+    function handleSuccess(stream) {
+        
+      var speechEvents = hark(stream);
+      speechEvents.setInterval(100);
+    speechEvents.on('speaking', function() {
+    //  notification.style.display = 'block';
+      //log('speaking');
+      console.log("---###Speaking----");
+      document.getElementById('other').classList.remove('green_background');
+      document.getElementById('speak').classList.add('green_background');
+    });
+
+    speechEvents.on('volume_change', function(volume, threshold) {
+     // log(volume, threshold)
+     // streamVolumes.push(volume);
+      //streamVolumes.shift();
+    });
+
+    speechEvents.on('stopped_speaking', function() {
+     // notification.style.display = 'none';
+     // log('stopped_speaking');
+     console.log("Stop speaking----");
+     document.getElementById('speak').classList.remove('green_background');
+     document.getElementById('other').classList.add('green_background');
+    
+    });
+    
+}
+function handleError(){
+    console.log("error");
+}
+} 
